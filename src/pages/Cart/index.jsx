f1f56e6468b5bfd9209da87afb94a517c1ab381a
daniel-sanchez9211 <dan_dbs@hotmail.com>
2020-04-05 20:18:25 -0300

    initial commit

---
+++ src/pages/Cart/index.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import {bindActionCreators} from 'redux'
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
+
+import * as CartActions from '../../store/modules/cart/actions'
+
+import { Container, ProductTable, Total } from './styles';
+
+function Cart({ cart, removeFromCart }) {
+   
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    {cart.map(product => (
+                        <tr key={product.id}>
+                            <td>
+                                <img src={product.image} alt={product.title} />
+                            </td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormated}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="button">
+                                        <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                    </button>
+                                    <input type="number" readOnly value={product.amount} />
+                                    <button type="button">
+                                        <MdAddCircleOutline size={20} color="#7159c1" />
+                                    </button>
+                                </div>
+                            </td>
+                            <td>
+                                <strong>R$300,00</strong>
+                            </td>
+                            <td>
+                                <button type="button" onClick={() => removeFromCart(product.id)}>
+                                    <MdDelete size={20} color="#7159c1" />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar pedido</button>
+
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$ 2000,00</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
+}
+
+const mapStateToProps = state => ({
+    cart: state.cart
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart)
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md'
+import { connect } from 'react-redux'
+import {bindActionCreators} from 'redux'
+
+import api from '../../services/api'
+
+import * as CartActions from '../../store/modules/cart/actions'
+
+import { formatPrice } from '../../util/format'
+import { ProductList } from './styles';
+
+class Home extends Component {
+    state = {
+        products: [],
+    }
+
+    async componentDidMount() {
+        const response = await api.get('/products')
+
+        const data = response.data.map(product => ({
+            ...product,
+            priceFormated: formatPrice(product.price)
+        }))
+
+        this.setState({ products: data })
+
+    }
+
+    handleAddProduct = (product) => {
+        const { addToCart } = this.props
+
+        addToCart(product)
+    }
+
+    render() {
+        const { products } = this.state
+
+        return (
+            <ProductList>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <img src={product.image} alt="Tênis" />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormated}</span>
+
+                        <button type="button" onClick={() => this.handleAddProduct(product)}>
+                            <div>
+                                <MdAddShoppingCart size={16} color="#fff" /> 3
+                            </div>
+
+                            <span>Adicionar no carrinho</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
+
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(Home)
